fe0352fecce07f371fb534958e8e645350798020
AerMoon-7 <a7autumn@outlook.com>
2024-10-01 21:29:53 +0800

    Initial commit

---
+++ src/main/java/name/autumn/RedStoneExtendClien.java
@@ -0,0 +1,10 @@
+package name.autumn;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RedStoneExtendClien implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
