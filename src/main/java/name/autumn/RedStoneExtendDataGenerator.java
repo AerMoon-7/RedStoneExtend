67903aeacbcbf02aad0832ab49bebb3db4b7934c
AerMoon-7 <a7autumn@outlook.com>
2024-10-02 21:31:14 +0800

    内容提交

---
+++ src/main/java/name/autumn/RedStoneExtendDataGenerator.java
@@ -0,0 +1,11 @@
+package name.autumn;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RedStoneExtendDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
